297dbe3b39b9f193d3c6481764a849fc5a610d30
higorvaluar <higorvb2001@gmail.com>
2024-05-17 17:41:25 -0300

    Atualização da aula 03 JAVA

--- aulas_Java/Programação Orientada a Objetos/java_aula3/src/Main.java
+++ aulas_Java/Programação Orientada a Objetos/java_aula3/src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         Moto moto_Higor = new Moto("Yamaha", "XTZ Crosser", "Branca", "QKI9673", 2017);
+        Moto moto_Gilsiandry = new Moto("Kawasaki","Ninja 250R","Verde","GIL0804",2009);
+        Moto moto_Hugo = new Moto("Yamaha","Factor","Vermelha","GAY2424", 2017);
         /*
         moto_Higor.setMarca("Yamaha");
         moto_Higor.setModelo("XTZ Crosser");
